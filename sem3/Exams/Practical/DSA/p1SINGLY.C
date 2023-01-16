f1ef225ad5088e95e5cbfc10d7e57e4e9f2f0b6b
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2023-01-17 00:22:04 +0530

    update 17 Jan 2023

--- sem3/Exams/Practical/DSA/p1SINGLY.C
+++ sem3/Exams/Practical/DSA/p1SINGLY.C
@@ -1,189 +1,171 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node{
     int data;
     struct node *next;
 }*head;
 
 void createList(int n);
-void insertNodeAtBeginning(int data);
-void insertNodeAtEnd(int data);
-void deleteNodeAtBeginning();
-void deleteNodeAtEnd();
 void displayList();
+void insertNodeAtBegining(int data);
+void insertNodeAtEnd(int data);
+void deleteNodeAtBegining();
+void deleteNodeAtEnd();
 
-int main()
-{
+int main(){
     int n, data, choice;
-
-    printf("Enter the total number of nodes: ");
-    scanf("%d", &n);
+    printf("\nEnter total number of nodes: ");
+    scanf("%d",&n);
     createList(n);
 
-    while(1)
-    {
-        printf("\n1. Insert node at beginning.\n");
-        printf("2. Insert node at end.\n");
-        printf("3. Delete node from beginning.\n");
-        printf("4. Delete node from end.\n");
-        printf("5. Display list.\n");
-        printf("6. Exit.\n");
-
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        if(choice==1)
-        {
-            printf("Enter data of first node: ");
-            scanf("%d", &data);
-            insertNodeAtBeginning(data);
-        }
-        else if(choice==2)
-        {
-            printf("Enter data of last node: ");
-            scanf("%d", &data);
-            insertNodeAtEnd(data);
-        }
-        else if(choice==3)
-        {
-            deleteNodeAtBeginning();
-        }
-        else if(choice==4)
-        {
-            deleteNodeAtEnd();
-        }
-        else if(choice==5)
-        {
-            displayList();
-        }
-        else
-        {
-            break;
+    while(1){
+        printf("\n1. Insert Node At Begining");
+        printf("\n2. Insert Node At End");
+        printf("\n3. Delete Node At Begining");
+        printf("\n4. Delete Node At End");
+        printf("\n5. Display List");
+        printf("\n6. Exit");
+        
+        printf("\n\nEnter Option: ");
+        scanf("%d",&choice);
+        
+        switch(choice){
+            case 1:
+                printf("\nEnter Data to be Inserted At Begining: ");
+                scanf("%d",&data);
+                insertNodeAtBegining(data);
+                break;
+            case 2:
+                printf("\nEnter Data to be Inserted At End: ");
+                scanf("%d",&data);
+                insertNodeAtEnd(data);
+                break;
+            case 3:
+                deleteNodeAtBegining();
+                break;
+            case 4: 
+                deleteNodeAtEnd();
+                break;
+            case 5:
+		displayList();
+                break;
+	    default:
+		clrscr();
+		return 0;
         }
     }
-
-    return 0;
+    
 }
 
-void createList(int n)
-{
-    struct node *newNode, *temp;
-    int data, i;
+void deleteNodeAtEnd(){
+    struct node *temp1, *temp2;
+    if(head == NULL){
+        printf("List is Already Empty!");
+    }
+    else if(head->next == NULL){
+        free(head);
+        head = NULL;
+    }
+    else{
+        temp1 = head;
+        while(temp1->next->next != NULL){
+            temp1 = temp1->next;    
+        }
+        temp2 = temp1->next;
+        temp1->next = NULL;
+        free(temp2);
+    }
+    printf("\nLast Node Deleted Successfully!");
+}
 
+
+
+void deleteNodeAtBegining(){
+    struct node *temp;
+    if(head == NULL){
+        printf("\nList is Already Empty!");
+        return;    
+    }
+    temp = head;
+    head = head->next;
+
+    free(temp);
+    printf("\nSuccessfully Deleted First Node!");
+}
+
+
+void insertNodeAtEnd(int data){
+    struct node *temp, *newNode;
+    newNode = (struct node *)malloc(sizeof(struct node));
+    
+    newNode->data = data;
+    newNode->next = NULL;
+    
+    if(head == NULL){
+        head = newNode;
+    }else{
+        temp = head;
+        while(temp->next != NULL){
+            temp=temp->next;    
+        }
+        temp->next = newNode;
+    }
+    printf("\nData inserted at End Successfully!");    
+}
+
+
+void insertNodeAtBegining(int data){
+    struct node *newNode;
+    newNode = (struct node *)malloc(sizeof(struct node));
+    
+    newNode->data = data;
+    newNode->next = head;
+    
+    head = newNode;
+    printf("\nData Succesfully Inserted at the Begining!");
+}
+
+void createList(int n){
+    struct node *temp, *newNode;
+    int i, data;
     head = (struct node *)malloc(sizeof(struct node));
-
-    printf("Enter the data of node 1: ");
-    scanf("%d", &data);
+    
+    printf("Enter data for node 1: ");
+    scanf("%d",&data);
 
     head->data = data;
     head->next = NULL;
-
+    
     temp = head;
-
-    for(i=2; i<=n; i++)
-    {
+    
+    for(i=2; i<=n; i++){
         newNode = (struct node *)malloc(sizeof(struct node));
-
-        printf("Enter the data of node %d: ", i);
-        scanf("%d", &data);
-
+        
+        printf("\nEnter data for node %d: ",i);
+        scanf("%d",&data);
+        
         newNode->data = data;
         newNode->next = NULL;
 
         temp->next = newNode;
         temp = temp->next;
+
     }
 }
 
-void insertNodeAtBeginning(int data)
-{
-    struct node *newNode;
-
-    newNode = (struct node*)malloc(sizeof(struct node));
-
-    newNode->data = data;
-    newNode->next = head;
-
-    head = newNode;
-
-    printf("Data inserted successfully at the beginning\n");
-}
-
-void insertNodeAtEnd(int data)
-{
-    struct node *newNode, *temp;
-
-    newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->data = data;
-    newNode->next = NULL;
-
+void displayList(){
+    struct node *temp;
+    if(head == NULL){
+        printf("\nList is Empty!");
+        return;    
+    }
     temp = head;
-
-    while(temp->next != NULL)
+    
+    while(temp!=NULL){
+        printf("\nData = %d",temp->data);
         temp = temp->next;
-
-    temp->next = newNode;
-
-    printf("Data inserted successfully at the end\n");
+    }
 }
 
-void deleteNodeAtBeginning()
-{
-struct node *temp;
-if(head == NULL)
-{
-    printf("List is already empty.\n");
-    return;
-}
-
-temp = head;
-head = head->next;
-
-printf("Data deleted successfully from the beginning\n");
-
-free(temp);
-}
-
-void deleteNodeAtEnd()
-{
-struct node *temp1, *temp2;
-if(head == NULL)
-{
-    printf("List is already empty.\n");
-    return;
-}
-
-temp1 = head;
-temp2 = head;
-
-while(temp1->next != NULL)
-{
-    temp2 = temp1;
-    temp1 = temp1->next;
-}
-
-temp2->next = NULL;
-
-printf("Data deleted successfully from the end\n");
-
-free(temp1);
-}
-
-void displayList()
-{
-struct node *temp;
-if(head == NULL)
-{
-    printf("List is empty.\n");
-    return;
-}
-
-temp = head;
-while(temp != NULL)
-{
-    printf("Data = %d\n", temp->data);
-    temp = temp->next;
-}
-}
 
